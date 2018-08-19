8149d608a2f1c4dd7afd7b94f33265d32fed1fa0
Paul Figiel <figielpaul@gmail.com>
2018-08-19 13:19:43 +0200

    perf tests and cleaning

--- src/index.js
+++ src/index.js
@@ -1,44 +1,40 @@
 // Core ////////////////////////////////////////////////////////////////////////////////////
-var Entity = class {
+var Entity = class
+{
     constructor()
     {
         // Generate a pseudo random ID
-        this.id = Entity.prototype._count;
-        
+        this.id = Entity.prototype.Count;
         // increment counter
-        Entity.prototype._count++;
-        
+        Entity.prototype.Count++;
         // The component data will live in this object
         this.components = {};
-        return this;
     }
     addComponent ( component ){
         // Add component data to the entity
         // NOTE: The component must have a name property (which is defined as 
         // a prototype protoype of a component function)
-        if(this.components[component.name]!=undefined)
-            return this;
-          this.components[component.name] = component;
-                for (let i = 0; i < System.AllSystems.length; i++) {
-                    const element = System.AllSystems[i];
-                    if(element.CanAdd(this))
-                    {
-                        element.subscribedEntities.push(this);
-                    }
-                }
-        return this;
+        if(!this.components[component.name]!=undefined)
+        {
+            this.components[component.name] = component;
+                  for (let i = 0; i < System.AllSystems.length; i++) {
+                      const element = System.AllSystems[i];
+                      if(element.CanAdd(this))
+                      {
+                          element.subscribedEntities.push(this);
+                      }
+                  }
+        }
     }
     removeComponent ( componentName ){
         // Remove component data by removing the reference to it.
         // Allows either a component function or a string of a component name to be
         // passed in
         var name = componentName; // assume a string was passed in
-    
         if(typeof componentName === 'function'){ 
             // get the name from the prototype of the passed component function
             name = componentName.prototype.name;
         }
-    
         for (let i = 0; i < System.AllSystems.length; i++) {
             const element = System.AllSystems[i];
             if(element.CanAdd(this))
@@ -46,7 +42,6 @@ var Entity = class {
                 element.subscribedEntities.splice(element.subscribedEntities.indexOf(this),1);
             }
         }
-    
         // Remove component data by removing the reference to it
         delete this.components[name];
         return this;
@@ -58,7 +53,7 @@ var Entity = class {
     }
 }
 // keep track of entities created
-Entity.prototype._count = 0;
+Entity.prototype.Count = 0;
 
 
 // Components ////////////////////////////////////////////////////////////////////////////////////
@@ -91,12 +86,23 @@ var System = class {
 }
 System.AllSystems = [];
 System.UpdateAll = ()=>System.AllSystems.every((val)=>val.Execute());
-let drawSystem = new System(e=>{console.log(e)},["size"]);
+let drawSystem = new System(e=>{e.components["size"].value*=2},["size"]);
 // Tests ////////////////////////////////////////////////////////////////////////////////////
 x=a.getContext`2d`;
 x.fillStyle = 'rgb(200,0,0)';
 x.fillRect(10,10,50,50);
-setInterval(e=>{ update() },33);
+var lastLoop = new Date;
+setInterval(e=>{ update();
+    for (let index = 0; index < 100; index++) {
+        let ent  = new Entity();
+        ent.addComponent(new Component(20,"size"));
+    }
+    console.log(e2.components["size"]);
+var thisLoop = new Date;
+var fps = 1000 / (thisLoop - lastLoop);
+console.log(fps + "  //  "+Entity.prototype.Count);
+lastLoop = thisLoop;
+},33);
 let e = new Entity();
 let e2 = new Entity();
 e.addComponent(new Component(10,"size"));
@@ -105,7 +111,7 @@ function update() {
 }
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 37) {
-        //e.removeComponent("size");
+        e.removeComponent("size");
         e.addComponent(new Component(1.0,"bounds"));
     }
     else if(event.keyCode == 39) {
